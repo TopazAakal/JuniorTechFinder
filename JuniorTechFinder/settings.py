396c1882ec199c44a685fc05dc559825b50b92ee
Afik Danan <Afikdanan@gmail.com>
2023-04-03 18:30:23 +0300

    Add: Postgres DB on AWS

--- JuniorTechFinder/settings.py
+++ JuniorTechFinder/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'JuniorTechFinder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'JuniorTechFinderDB',
+        'USER': 'JTF_Admin',
+        'PASSWORD': 'Pa55w0rd',
+        'HOST': 'juniortechfinder.ccjbdlszbyi4.eu-north-1.rds.amazonaws.com',
+        'PORT': '54324',
     }
 }
 
