283e5a19e56445ea819ebd06f2f477b399bc31da
Topaz Aakal <topazaakal5@gmail.com>
2023-04-16 18:20:08 +0300

    BSPMC235-8 adding unit tests for Auth app

--- Authentication/tests.py
+++ Authentication/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class LogoutViewTest(TestCase):
+    def setUp(self):
+        # create a test client and a test user and log the user in.
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='testuser@test.com',
+            password='testpassword'
+        )
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_logout_view(self):
+        # send a GET request to the logout view.
+        response = self.client.get(reverse('logout'))
+        # assert that the HTTP response status code is 302 (a redirect)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('home'))
+        # assert that the user is logged out.
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
