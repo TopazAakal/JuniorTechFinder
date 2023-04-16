6f0a7ac9a939b11b0618833d741230494735ebaf
Afik Danan <Afikdanan@gmail.com>
2023-04-16 12:02:20 +0300

    BSPMC235-5 login page functionality - Done

--- JuniorTechFinder/urls.py
+++ JuniorTechFinder/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import Core.views
+import Authentication.views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', Core.views.homePage, name='home'),
+    path('admin/', admin.site.urls),
+    path('login/', Authentication.views.login_view, name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
