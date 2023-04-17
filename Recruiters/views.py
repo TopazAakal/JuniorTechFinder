f2b6febdf3510abd0aa7e9e289e47038582a45de
mormer1 <morme1@ac.sce.ac.il>
2023-04-17 10:03:52 +0300

    BSPMC235-6

--- Recruiters/views.py
+++ Recruiters/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib import messages
+from .models import Recuiters
+from .forms import RecruitersForm
 
-# Create your views here.
+
+def createProfile(request):
+    if request.method == 'POST':
+        form = RecruitersForm(request.POST, request.FILES)
+        if form.is_valid():
+            try:
+                recuiter = form.save()  # save the new Junior instance to the database
+                # redirect to the detail page for the new Junior instance
+                return redirect('showProfile', pk=recuiter.pk)
+            except Exception as e:
+                messages.error(request, f"Error saving form: {e}")
+        else:
+            print(form.errors)
+            messages.error(request, "Form is not valid.")
+    else:
+        form = RecruitersForm()
+    return render(request, 'createProfile.html', {'form': form})
+
+
+def showProfile(request, pk):
+    # retrieve the Junior instance with the given primary key, or return a 404 error
+    recuiter = get_object_or_404(Recuiters, pk=pk)
+    return render(request, 'showProfile.html', {'recuiter': recuiter})
