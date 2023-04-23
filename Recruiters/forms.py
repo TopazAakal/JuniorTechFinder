8f82baaf720e36ad07bc5e25cb990a1f4ac0a412
mormer1 <morme1@ac.sce.ac.il>
2023-04-23 22:13:30 +0300

    some fix

--- Recruiters/forms.py
+++ Recruiters/forms.py
@@ -1,13 +1,15 @@
 from django import forms
 from .models import Recruiters
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class RecruitersForm(forms.ModelForm):
+    user = forms.IntegerField(widget=forms.HiddenInput(), required=True)
     class Meta:
         model = Recruiters
         fields = ['full_name', 'email', 'phone_number', 'city',
-                  'age', 'summary', 'company', 'photo']
+                  'age', 'company', 'summary', 'photo']
         widgets = {
             'full_name': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
@@ -18,3 +20,9 @@ class RecruitersForm(forms.ModelForm):
             'summary': forms.Textarea(attrs={'class': 'form-control'}),
             'photo': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
+
+    def __init__(self, *args, **arg):
+        user = arg.pop('user', None)
+        super().__init__(*args, **arg)
+        if user:
+            self.fields['user'].initial = user.id
--- Recruiters/views.py
+++ Recruiters/views.py
@@ -2,38 +2,39 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib import messages
 from .models import Recruiters
 from .forms import RecruitersForm
-
+from django import forms
 
 def createProfileRecruiters(request):
+    user = request.user
+    if hasattr(user, 'recruiters'):
+        return redirect('checkProf')
+
     if request.method == 'POST':
-        form = RecruitersForm(request.POST, request.FILES)
+        form_data = request.POST.copy()  
+        form_data['user'] = request.user.id  
+        form = RecruitersForm(form_data, request.FILES)
         if form.is_valid():
-            try:
-                recruiter = form.save()  # save the new Junior instance to the database
-                # redirect to the detail page for the new Junior instance
-                return redirect('showProfileRecruiter.html', pk=recruiter.pk)
-            except Exception as e:
-                messages.error(request, f"Error saving form: {e}")
-        else:
-            print(form.errors)
-            messages.error(request, "Form is not valid.")
+            recruiter = form.save(commit=False)
+            recruiter.user = user
+            recruiter.save()
+            return redirect('checkProf')
     else:
         form = RecruitersForm()
+        form.fields['user'].widget = forms.HiddenInput()
+        form.fields['user'].initial = user.id
+
     return render(request, 'createProfileRecruiters.html', {'form': form})
 
 def showProfileRecruiter(request, pk):
-    recruiter = get_object_or_404(Recruiters, pk=pk)
+    recruiter = get_object_or_404(Recruiters, pk=pk, user=request.user)
+    default_photo_url = '/static/media/default.jpg'
+    context = {'recruiter': recruiter, 'default_photo_url': default_photo_url}
+    return render(request, 'showProfileRecruiter.html', context)
 
-    if request.method == 'POST':
-        recruiter.full_name = request.POST.get('full_name')
-        recruiter.email = request.POST.get('email')
-        recruiter.phone_number = request.POST.get('phone_number')
-        recruiter.city = request.POST.get('city')
-        recruiter.age = request.POST.get('age')
-        recruiter.summary = request.POST.get('summary')
-        recruiter.company = request.POST.get('company')
-        if request.FILES.get('photo'):
-            recruiter.photo = request.FILES.get('photo')       
-        recruiter.save()
 
-    return render(request, 'showProfileRecruiter.html', {'recruiter': recruiter})
+def checkProf(request):
+    try:
+        recruiter = request.user.recruiters
+        return redirect('showProfileRecruiter', pk=recruiter.pk)
+    except Recruiters.DoesNotExist:
+        return redirect('createProfileRecruiters')
