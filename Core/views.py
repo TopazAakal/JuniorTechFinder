6bb125a7bea18fa8ce1aae5a4e5b26beb74863cf
Topaz Aakal <topazaakal5@gmail.com>
2023-04-28 20:50:07 +0300

    BSPMC235-30 contact us page for juniors

--- Core/views.py
+++ Core/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
 
 
 def homePage(request):
     return render(request, 'home.html')
 
+
 def aboutUsPage(request):
     return render(request, 'aboutUs.html')
 
+
 def contactUsPage(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+        print(f'Name: {name}\nEmail: {email}\nMessage: {message}')
+        messages.success(request, 'Your message has been sent successfully!')
+        return render(request, 'contactUs.html')
     return render(request, 'contactUs.html')
-
-
