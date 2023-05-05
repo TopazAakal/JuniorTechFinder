ca934855de57c9256c85c3431d560e21392bc377
Tala1230 <Talha13@ac.sce.ac.il>
2023-05-05 14:37:04 +0300

    BSPMC235-35 Site Rules Update

--- Authentication/forms.py
+++ Authentication/forms.py
@@ -1,20 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth.models import Group
 
 
 # LoginForm class for user login form
-
-
 class LoginForm(forms.Form):
     # Form fields for username and password
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
 
 # SignUpForm class for user signup form, extending UserCreationForm
-
-
 class SignUpForm(UserCreationForm):
     # Form fields for email, first name, last name, password1 and password2
     email = forms.EmailField(
@@ -23,13 +18,11 @@ class SignUpForm(UserCreationForm):
         max_length=30, required=True, help_text='Required.')
     last_name = forms.CharField(
         max_length=30, required=True, help_text='Required.')
-    role = forms.ModelChoiceField(queryset=Group.objects.all(), required=True)
 
     class Meta:
         # Set model as User and include required fields
         model = User
-        fields = ('email', 'first_name', 'last_name',
-                  'role', 'password1', 'password2')
+        fields = ('email', 'first_name', 'last_name', 'password1', 'password2')
         # Set widgets for each field for class styling
         widgets = {
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
@@ -37,7 +30,6 @@ class SignUpForm(UserCreationForm):
             'last_name': forms.TextInput(attrs={'class': 'form-control'}),
             'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
             'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
-            'role': forms.Select(attrs={'class': 'form-control'}),
         }
 
     def clean(self):
