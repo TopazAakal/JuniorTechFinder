0940cae8d27974b5f060896cbd609199c4eac2e2
Topaz Aakal <topazaakal5@gmail.com>
2023-05-18 16:56:34 +0300

    BSPMC235-45 create JuniorList page and search func

--- JuniorTechFinder/urls.py
+++ JuniorTechFinder/urls.py
@@ -1,18 +1,4 @@
-"""JuniorTechFinder URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 import Core.views
@@ -32,14 +18,13 @@ urlpatterns = [
     path('showProfile/<int:pk>', Juniors.views.showProfile, name='showProfile'),
     path('checkProfile/', Juniors.views.checkProfile, name='checkProfile'),
     path('editProfile/<int:pk>', Juniors.views.editProfile, name='editProfile'),
+    path('juniorList/', Juniors.views.juniorList, name='juniorList'),
     path('showProfileRecruiter/<int:pk>',
          Recruiters.views.showProfileRecruiter, name='showProfileRecruiter'),
     path('editProfileRecruiter/<int:pk>',
          Recruiters.views.editProfileRecruiter, name='editProfileRecruiter'),
     path('createProfileRecruiters/', Recruiters.views.createProfileRecruiters,
          name='createProfileRecruiters'),
-    # path('checkProfileUser/', Authentication.views.checkProfileUser,
-    # name='checkProfileUser'),
     path('checkProf/', Recruiters.views.checkProf, name='checkProf'),
     path('jobs/', Recruiters.views.jobList, name='jobList'),
     path('postJob/', Recruiters.views.postJob, name='postJob'),
--- Juniors/admin.py
+++ Juniors/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Juniors
 
-# Register your models here.
+
+class JuniorsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'email', 'phone_number', 'city', 'age')
+    list_filter = ('city', 'age')
+    search_fields = ('full_name', 'email', 'phone_number')
+
+
+admin.site.register(Juniors, JuniorsAdmin)
