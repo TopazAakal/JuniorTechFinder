7126d53a7cebef43b9859978e11fe76cbe9819bf
Afik Danan <Afikdanan@gmail.com>
2023-09-10 15:54:16 +0300

    afik

--- JuniorTechFinder/settings.py
+++ JuniorTechFinder/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5t@hwj5w@$=$#j0+qu9@)%m!bq052y)w21tk+3gg_4rka8ccr0'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
